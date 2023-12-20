d2a2c74b6736cbacf9ba56d6e8ab01d5f24a3999
wreegp <124398959+wreegp@users.noreply.github.com>
2023-12-20 14:36:00 -0700

    Add files via upload

---
+++ Memory Game - Tutorial/main.js
@@ -0,0 +1,13 @@
+let errors = 0;
+let cardList = [
+    "darkness",
+    "double", 
+    "fairy",
+    "lightning",
+    "fire", 
+    "grass", 
+    "lightning",
+    "metal", 
+    "psychic",
+    "water", 
+] 
