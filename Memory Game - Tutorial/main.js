eb5d1673f1d0b3c494f960fdfa5609e7b6194379
wreegp <124398959+wreegp@users.noreply.github.com>
2023-12-21 14:46:11 -0700

    Add files via upload

--- Memory Game - Tutorial/main.js
+++ Memory Game - Tutorial/main.js
@@ -1,13 +1,65 @@
-let errors = 0;
-let cardList = [
+var errors = 0;
+var cardList = [
     "darkness",
-    "double", 
+    "double",
     "fairy",
+    "fighting",
+    "fire",
+    "grass",
     "lightning",
-    "fire", 
-    "grass", 
-    "lightning",
-    "metal", 
+    "metal",
     "psychic",
-    "water", 
-] 
+    "water"
+]
+
+
+var cardSet;
+var board = [];
+var rows = 4;
+var columns =5;
+
+var card1Selected;
+var card2Selected;
+
+window.onload = function() {
+    shuffleCards();
+    startGame();
+}
+
+function shuffleCards() {
+    cardSet = cardList.concat(cardList); //two of each card
+    console.log(cardSet);
+    //shuffle
+    for (let i = 0; i < cardSet.length; i++) {
+        let j = Math.floor(Math.random() * cardSet.length); //get random index
+        //swap
+        let temp = cardSet[i];
+        cardSet[i] = cardSet[j];
+        cardSet[j] = temp;
+    }
+    console.log(cardSet);
+}
+
+function startGame() {
+    //arrange the board 4x5
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            let cardImg = cardSet.pop();
+            row.push(cardImg); //JS
+
+            // <img id="0-0" class="card" src="water.jpg">
+            let card = document.createElement("img");
+            card.id = r.toString() + "-" + c.toString();
+            card.src = cardImg + ".jpg";
+            card.classList.add("card");
+            document.getElementById("board").append(card);
+
+        }
+        board.push(row);
+    }
+
+    console.log(board);
+
+}
+
