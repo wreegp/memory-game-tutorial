df0799aa1851ecb4292fad518caf2036cbe261fc
wreegp <124398959+wreegp@users.noreply.github.com>
2024-01-08 14:21:35 -0700

    Add files via upload

--- Memory Game - Tutorial/main.js
+++ Memory Game - Tutorial/main.js
@@ -53,6 +53,7 @@ function startGame() {
             card.id = r.toString() + "-" + c.toString();
             card.src = cardImg + ".jpg";
             card.classList.add("card");
+            card.addEventListener("click", selectCard);
             document.getElementById("board").append(card);
 
         }
@@ -60,6 +61,53 @@ function startGame() {
     }
 
     console.log(board);
+    setTimeout(hideCards, 1000);
+}
+
+function hideCards() {
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns; c++) {
+            let card = document.getElementById(r.toString() + "-" + c.toString());
+            card.src = "back.jpg";
+        }
+    }
+}
+
+function selectCard() {
+
+    if (this.src.includes("back")) {
+        if (!card1Selected) {
+            card1Selected = this;
+
+            let coords = card1Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card1Selected.src = board[r][c] + ".jpg";
+        }
+        else if (!card2Selected && this != card1Selected) {
+            card2Selected = this;
+
+            let coords = card2Selected.id.split("-"); //"0-1" -> ["0", "1"]
+            let r = parseInt(coords[0]);
+            let c = parseInt(coords[1]);
+
+            card2Selected.src = board[r][c] + ".jpg";
+            setTimeout(update, 1000);
+        }
+    }
 
 }
 
+function update() {
+    //if cards aren't the same, flip both back
+    if (card1Selected.src != card2Selected.src) {
+        card1Selected.src = "back.jpg";
+        card2Selected.src = "back.jpg";
+        errors += 1;
+        document.getElementById("errors").innerText = errors;
+    }
+
+    card1Selected = null;
+    card2Selected = null;
+}
